d63e038df1ca2b87d7f29efa389f17907ac0c9dd
Mehmet ALPEREN <me.alperen@gmail.com>
2022-06-05 23:01:30 +0300

    Footer refactored.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,131 +1,118 @@
 import {
   FaChevronDown,
+  FaChevronUp,
   FaSpotify,
   FaFacebook,
   FaPinterest,
   FaInstagram,
   FaYoutube,
-  FaTwitter,
-} from 'react-icons/fa';
+  FaTwitter
+} from 'react-icons/fa'
+import { useState } from 'react'
+
+const footerLinks = [
+  {
+    id: 1,
+    title: 'about us',
+    links: [
+      'our company',
+      'our coffee',
+      'stories and news',
+      'starbucks archive',
+      'investor relations',
+      'customer services'
+    ]
+  },
+  {
+    id: 2,
+    title: 'about us',
+    links: [
+      'our company',
+      'our coffee',
+      'stories and news',
+      'starbucks archive',
+      'investor relations',
+      'customer services'
+    ]
+  }
+]
+
+const Accordion = () => {
+  const [showLinks, setShowLinks] = useState(false)
+
+  return (
+    <div>
+      <div>
+        about us
+        <button onClick={() => setShowLinks(!showLinks)}>
+          {showLinks ? <FaChevronUp /> : <FaChevronDown />}
+        </button>
+      </div>
+      <ul>
+        {showLinks && (
+          <div>
+            <li>our company</li>
+            <li>our coffee</li>
+            <li>stories and news</li>
+            <li>starbucks archive</li>
+            <li>investor relations</li>
+            <li>customer services</li>
+          </div>
+        )}
+      </ul>
+    </div>
+  )
+}
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <div className='about-us'>
-        <h3>
-          about us
-          <span>
-            <FaChevronDown />
-          </span>
-        </h3>
-        <ul>
-          <li>our company</li>
-          <li>our coffee</li>
-          <li>stories and news</li>
-          <li>starbuks archive</li>
-          <li>investor relations</li>
-          <li>customer services</li>
-        </ul>
+    <div className="footer">
+      <div className="">
+        {footerLinks.map((item) => {
+          return <Accordion {...item} key={item.id} />
+        })}
       </div>
-      <div className='careers'>
-        <h3>
-          careers
-          <span>
-            <FaChevronDown />
-          </span>
-        </h3>
-        <ul>
-          <li>culture and values</li>
-          <li>inclusion, diversity and equity</li>
-          <li>college achievement plan</li>
-          <li>alumni community</li>
-          <li>U.S. careers</li>
-          <li>international careers</li>
-        </ul>
-      </div>
-      <div className='social-impact'>
-        <h3>
-          social-impact
-          <span>
-            <FaChevronDown />
-          </span>
-        </h3>
-        <ul>
-          <li>people</li>
-          <li>planet</li>
-          <li>environmental and social reporting</li>
-        </ul>
-      </div>
-      <div className='for-business-partners'>
-        <h3>
-          for business partners
-          <span>
-            <FaChevronDown />
-          </span>
-        </h3>
-        <ul>
-          <li>landlord support center</li>
-          <li>suppliers</li>
-          <li>corporote gift card sales</li>
-          <li>office and foodservice coffee</li>
-        </ul>
-      </div>
-      <div className='order-and-pickup'>
-        <h3>
-          order and pickup
-          <span>
-            <FaChevronDown />
-          </span>
-        </h3>
-        <ul>
-          <li>order on the app</li>
-          <li>order on the web</li>
-          <li>delivery</li>
-          <li>order and pickup options</li>
-          <li>explore and find coffee for home</li>
-        </ul>
-      </div>
-      <div className='footer-icons'>
-        <a href=''>
+      <div className="footer-icons">
+        <a href="">
           <FaSpotify />
         </a>
-        <a href=''>
+        <a href="">
           <FaFacebook />
         </a>
-        <a href=''>
+        <a href="">
           <FaPinterest />
         </a>
-        <a href=''>
+        <a href="">
           <FaInstagram />
         </a>
-        <a href=''>
+        <a href="">
           <FaYoutube />
         </a>
-        <a href=''>
+        <a href="">
           <FaTwitter />
         </a>
       </div>
-      <div className='company-info'>
+      <div className="company-info">
         <ul>
           <li>
-            <a href=''>privacy policy</a>
+            <a href="">privacy policy</a>
           </li>
           <li>
-            <a href=''>terms of use</a>
+            <a href="">terms of use</a>
           </li>
           <li>
-            <a href=''>CA supply chain act</a>
+            <a href="">CA supply chain act</a>
           </li>
           <li>
-            <a href=''>cookie preferences</a>
+            <a href="">cookie preferences</a>
           </li>
         </ul>
       </div>
-      <div className='rights-year'>
+      <div className="rights-year">
         <p>© 2022 Starbucks Coffee Company. All rights reserved.</p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
