97e976bfe026f8e8d694e559cf66fd71a4fbcf0a
Mehmet ALPEREN <me.alperen@gmail.com>
2022-06-08 16:56:33 +0300

    Sidebar finished. Box shadow will be fixed.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { useGlobalContext } from '../context'
+import {
+  FaMapMarkerAlt,
+  FaAlignJustify,
+  FaRegWindowClose
+} from 'react-icons/fa'
 const Sidebar = () => {
   const { isSidebarOpen } = useGlobalContext()
 
@@ -10,7 +15,39 @@ const Sidebar = () => {
   }, [isSidebarOpen])
   return (
     <aside className={`${isSidebarOpen ? 'sidebar show-sidebar ' : 'sidebar'}`}>
-      <div className="sidebar-container">Inside Sidebar</div>
+      <div className="sidebar-container">
+        <div className="sidebar-content">
+          <ul className="sidebar-links">
+            <li>
+              <a href="">Menu</a>
+            </li>
+            <li>
+              <a href="">Rewards</a>
+            </li>
+            <li>
+              <a href="">Gift Cards</a>
+            </li>
+          </ul>
+          <ul className="sidebar-buttons">
+            <li>
+              <a href="" className="btn white-btn">
+                Sign in
+              </a>
+            </li>
+            <li>
+              <a href="" className="btn black-btn">
+                Join now
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <FaMapMarkerAlt className="icon-map" />
+                <span>Find a store</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </aside>
   )
 }
